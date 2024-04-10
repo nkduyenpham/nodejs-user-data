1da018c61ed7b5c37e8444930c9cac5bd41350a9
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-04-11 00:04:38 +0800

    update crud

--- routes/users.js
+++ routes/users.js
@@ -25,35 +25,80 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify({users},null,4));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
+
+// commandline to query this curl localhost:5000/user/johnsmith@gamil.com
+
 router.get("/:email",(req,res)=>{
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filtered_users = users.filter((user) => user.email === email);
+  res.send(filtered_users)
 });
 
 
 // POST request: Create a new user
+// curl localhost:5000/user/jonlovato@theworld.com
+
+
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  users.push({
+    "firstName": req.query.firstName,
+    "lastName": req.query.lastName,
+    "email":req.query.email,
+    "DOB":req.query.DOB
+  });
+  res.send("The user" + (' ') + (req.query.firstName) + "Has been added!")
 });
 
 
 // PUT request: Update the details of a user by email ID
+// cmd to check curl --request PUT 'localhost:5000/user/johnsmith@gamil.com?DOB=1/1/1971'
+// curl localhost:5000/user/johnsmith@gamil.com
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filtered_users = users.filter((user) => user.email === email)
+  if (filtered_users.length > 0) {
+    let filtered_user = filtered_users[0];
+    let DOB = req.query.DOB;
+    // if the DOB has changed
+    if (DOB) {
+      filtered_user = DOB
+    };
+    // if you want to check for another atribute, add the same code
+    users = users.filter((user) => user.email != email);
+    users.push(filtered_user);
+    res.send(`User with the email ${email} updated`);
+  } else {
+    res.send("Unable to find user!")
+  }
 });
 
 
 // DELETE request: Delete a user by email ID
+// curl --request DELETE 'localhost:5000/user/johnsmith@gamil.com'
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  users = users.filter((user) => user.email != email);
+  res.send(`User with the email ${email} deleted`)
+});
+
+function getDateFromString(strDate) {
+  let [dd,mm,yyyy] = strDate.split('-')
+  return new Date(yyyy+"/"+mm+"/"+dd);
+}
+  
+// console.log(sorted_users);
+router.get("/sort",(req,res)=>{
+  let sorted_users=users.sort(function(a, b) {
+      let d1 = getDateFromString(a.DOB);
+      let d2 = getDateFromString(b.DOB);
+          return d1-d2;
+        });
+  res.send(sorted_users);
 });
 
 module.exports=router;
